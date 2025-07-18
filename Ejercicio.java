dc804beaa8f5002442958e9eed6a592e4f1eb658
FabsAlfaro <fabiola.alfaro.mtz@hotmail.com>
2025-07-18 17:12:11 -0600

    Add files via upload
    Ejercicios de Exámen

---
+++ Ejercicio.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio;
+import java.util.Scanner;
+
+public class Ejercicio {
+   
+    public static void evaluarRango(int inicio, int fin){   
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Ingresa el rango Inicial: ");
+        inicio = entrada.nextInt();
+        
+        System.out.print("Ingresa el rango Final: ");
+        fin = entrada.nextInt();
+        
+        for(int i=inicio; i<=fin; i++ ){
+            double fx = evaluarFuncion(i);
+            System.out.println("f(x)" + fx);
+        }       
+    }
+     
+    public static double evaluarFuncion(int x) { 
+        double resultado = (x*x)- 3 * x + 2;
+        return resultado;       
+    }
+   
+    public static void main(String[] args) {
+
+        evaluarRango(2,5);
+        
+    }
+}
